641f3d3577ef09bc347e493bb4364ccd9a2faab3
Nolan Neeley <nolanneeley@gmail.com>
2018-03-27 19:52:56 -0500

    changed db from sqlite to postgres for deployment to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180323141218) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "description"
